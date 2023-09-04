c8c7ebf0f91a20a7169bdac9daec0b92ef1c5e43
oo0o00oo0 <dsgnrm@gmail.com>
2023-09-04 23:33:30 +0100

    pp test

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,14 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import { Grid_1 } from "@src/layout/layout"
 
 type Props = {}
 
 const About = (props: Props) => {
+   const [isImageLoaded, setImageLoaded] = useState(false)
+
+   const ref = React.useRef<HTMLImageElement>(null)
+
    return (
       <Grid_1>
          <Header>ABOUT123</Header>
-         <img src="/atlas/atlas.webp"></img>
+
+         <div
+            style={{
+               top: "40px",
+               position: "absolute"
+            }}>
+            <div
+               style={{
+                  top: "400px",
+                  position: "absolute",
+                  height: "400px",
+                  width: "400px"
+               }}>
+               <img
+                  style={{
+                     opacity: isImageLoaded ? 1 : 0,
+                     transition: "opacity 1s ease-in-out",
+                     position: "absolute"
+                  }}
+                  ref={ref}
+                  src="/atlas/atlas.webp"
+                  onLoad={e =>
+                     setTimeout(() => {
+                        setImageLoaded(true)
+                     }, 1000)
+                  }
+                  alt="Atlas"
+               />
+            </div>
+            {!isImageLoaded && (
+               <LoadingPlaceholder
+                  style={{
+                     top: "400px",
+                     position: "absolute",
+                     height: "400px",
+                     width: "400px"
+                  }}>
+                  Loading image...
+               </LoadingPlaceholder>
+            )}
+         </div>
       </Grid_1>
    )
 }
@@ -16,6 +60,15 @@ const About = (props: Props) => {
 const Header = styled.h1`
    grid-area: header;
    font-family: WONKY;
+`
+
+const LoadingPlaceholder = styled.div`
+   width: 100%; // Adjust according to your needs
+   height: 300px; // Adjust according to your needs
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   background-color: #e0e0e0; // Placeholder background
 `
 
 export default About
